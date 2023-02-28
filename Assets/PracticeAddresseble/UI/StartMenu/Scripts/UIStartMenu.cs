81997e1f15df479821ffacddb947ffddec1e1a02
dvpavlov-dev <40720296+dvpavlov-dev@users.noreply.github.com>
2023-02-28 23:47:20 +0300

    Поменял название на стартовом экране
    1) Поменял название на стартовом экране
    2) Подправил размеры панелей на уровнях (еще нужно откалибровать 2-й и 3-й уровень под широкоформатный экран)

--- Assets/PracticeAddresseble/UI/StartMenu/Scripts/UIStartMenu.cs
+++ Assets/PracticeAddresseble/UI/StartMenu/Scripts/UIStartMenu.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 public class UIStartMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _startPanel;
+    [SerializeField] private GameObject _loadingPanel;
+    [SerializeField] private GameObject _loadingImg;
     [SerializeField] private AudioSource _audioSource;
 
     public void StartGame()
@@ -28,6 +30,8 @@ public class UIStartMenu : MonoBehaviour
 
     private async Task ChangeLevelAsync()
     {
+        _loadingPanel.SetActive(true);
+        _loadingImg.transform.DORotate(new Vector3(0, 0, 360), 5, RotateMode.LocalAxisAdd).SetLoops(-1).SetEase(Ease.Linear);
         var asyncOperationHandle = Addressables.LoadSceneAsync($"Level 1");
         await asyncOperationHandle.Task;
     }
